6d0f77b61b56aeaf4446f45f0f36a88fe9a79d5e
1makki <yazkibay2@gmail.com>
2023-02-08 04:29:07 +0700

    ddl and dml

---
+++ PART2-DDL.sql
@@ -0,0 +1,136 @@
+-- no.1
+create database alta_online_shop;
+
+-- no.2
+use alta_online_shop;
+drop table users;
+-- 2-a
+create table users(
+id int primary key not null auto_increment,
+name varchar(50),
+gender enum('M','F'),
+Stats enum('active','Inactive'),
+created_at datetime default current_timestamp,
+updated_at datetime default current_timestamp,
+deleted_at datetime
+);
+
+-- 2-b
+create table products(
+id int primary key not null auto_increment,
+name varchar(50),
+price decimal,
+product_type_id int,
+created_at datetime default current_timestamp,
+updated_at datetime default current_timestamp,
+constraint fk_Product_type foreign key (product_type_id) 
+references product_types(id)
+);
+
+create table Product_types(
+id int primary key not null auto_increment,
+categories varchar(50),
+created_at datetime default current_timestamp,
+updated_at datetime default current_timestamp
+);
+
+create table Product_descriptions(
+Product_id int primary key not null auto_increment,
+descriptions text,
+created_at datetime,
+updated_at datetime,
+constraint fk_product_descriptions foreign key (product_id) 
+references products(id)
+);
+
+create table payment_methods(
+id int primary key not null auto_increment,
+name varchar(50),
+created_at datetime,
+updated_at datetime
+);
+
+-- 2-c
+create table transactions(
+id int primary key not null auto_increment,
+user_id int,
+total_qty int,
+total_transaction decimal,
+transcaction_date datetime,
+payment_method_id int,
+created_at datetime,
+updated_at datetime,
+constraint fk_user foreign key (user_id) 
+references users(id),
+constraint fk_payment_method foreign key (payment_method_id) 
+references payment_methods(id)
+);
+
+create table transaction_details(
+id int primary key not null auto_increment,
+transaction_id int,
+Product_id int,
+price decimal,
+qty int,
+created_at datetime,
+updated_at datetime,
+constraint fk_transaction foreign key (transaction_id) 
+references transactions(id),
+constraint fk_product foreign key (product_id) 
+references products(id)
+);
+
+show tables from alta_online_shop;
+show columns from products;
+
+-- no.3
+
+create table kurir(
+id int ,
+name varchar(50),
+created_at datetime,
+updated_at datetime
+);
+
+-- no.4
+
+alter table kurir
+add ongkos_dasar int;
+
+-- no.5 
+Rename table kurir to shipping;
+
+-- no.6 
+drop table shipping;
+
+-- no.7
+-- 7a
+create table payment_method_descriptions(
+payment_method_id int primary key not null auto_increment,
+description varchar(255),
+created_at datetime,
+updated_at datetime,
+constraint fk_payment_method_id foreign key (payment_method_id) 
+references payment_methods(id)
+);
+
+-- 7b
+create table address(
+id int primary key,
+user_id int,
+adress varchar(255),
+constraint fk_user_id foreign key (user_id) 
+references users(id)
+);
+
+-- 7c
+create table user_payment_method_detail(
+id int primary key not null auto_increment,
+user_id int,
+payment_method_id int,
+detail varchar(255),
+constraint fk_user_detail foreign key (user_id) 
+references users(id),
+constraint fk_payment_method_detail foreign key (payment_method_id) 
+references payment_methods(id)
+);
---
+++ PART3-DML.sql
@@ -0,0 +1,97 @@
+use alta_online_shop;
+-- no.1
+-- 1-a
+insert into product_types (id, categories)
+values ("1","alat makan");
+insert into product_types (id, categories)
+values ("2","eletronik");
+insert into product_types (id, categories)
+values ("3","kosmetik");
+-- 1-b
+insert into products (id, name , price, product_type_id)
+values ("1","sendok","5000","1"),
+("2","garpu","5000","1");
+-- 1-c
+insert into products (id, name , price, product_type_id)
+values ("3","TV","1000000","2"),
+("4","radio","500000","2"),
+("5","kulkas","800000","2");
+-- 1-d
+insert into products (id, name , price, product_type_id)
+values ("6","wardah","35000","3"),
+("7","kahf","45000","3"),
+("8","garnier","38000","3");
+-- 1-e
+insert into product_descriptions (product_id, descriptions)
+values ("1","alat makan"),
+("2","alat makan"),
+("3","elektronik"),
+("4","elektronik"),
+("5","elektronik"),
+("6","kosmetik wanita"),
+("7","kosmetik pria"),
+("8","kosmetik pria");
+-- 1-f
+insert into payment_methods (id, name)
+values ("1","Transfer bank"),
+("2","virtual account"),
+("3","E-money");
+-- 1-g
+insert into users (id, name, gender , stats)
+values ("1","Asep","M","active"),
+("2","Frida","F","active"),
+("3","Agus","M","inactive"),
+("4","Fahmi","M","active"),
+("5","Rumini","F","active");
+-- 1-h
+insert into transactions (id, user_id, total_qty , total_transaction, transaction_date, payment_method_id)
+values ("1","2","3","105000", "2023-02-08 15:01:27", "1"),
+("2","5","3","3000000", "2023-02-08 13:01:27", "3"),
+("3","1","3","240000", "2023-02-08 19:01:27", "2"),
+("4","2","3","3000000", "2023-02-08 15:01:27", "1"),
+("5","2","3","240000", "2023-02-08 15:01:27", "1"),
+("6","5","3","105000", "2023-02-08 15:01:27", "3"),
+("7","5","3","240000", "2023-02-08 15:01:27", "3"),
+("8","1","3","105000", "2023-02-08 15:01:27", "2"),
+("9","1","3","3000000", "2023-02-08 15:01:27", "2");
+-- 1-i
+insert into transaction_details (transaction_id, product_id, Price , qty)
+values ("1","6","35000","3"),
+("2","3","1000000","3"),
+("3","5","800000","3"),
+("4","6","1000000","3"),
+("5","6","80000","3"),
+("6","3","35000","3"),
+("7","3","80000","3"),
+("8","5","35000","3"),
+("9","5","1000000","3");
+
+-- no.2
+-- 2-a
+select id,name,gender from users where gender="M";
+-- 2-b
+select id,name,price,Product_type_id from products where id="3";
+-- 2-c
+select * from users where created_at > date_sub(now(), interval 1 week) 
+and name like '%a%';
+-- 2-d
+select count(*) from users where gender="F";
+-- 2-e
+select * from users order by name;
+-- 2-f
+select * from transaction_details where product_id="3";
+
+-- no.3
+-- 3-a
+update products set name = "product dummy" where id="1";
+-- 3-b
+update transaction_details set qty = "3" where product_id="1";
+
+-- no.4
+-- 4-a
+delete from products where id="1";
+-- 4-b
+delete from products where product_type_id="1";
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
